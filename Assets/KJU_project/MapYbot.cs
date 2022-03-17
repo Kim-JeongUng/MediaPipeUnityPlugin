843998456db4d1c44f496cc9c2cdccee675f2586
Kim JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2022-03-17 11:51:17 +0900

    leg test

--- Assets/KJU_project/MapYbot.cs
+++ Assets/KJU_project/MapYbot.cs
@@ -12,9 +12,17 @@ public class MapYbot : MonoBehaviour
   public Transform elbowLeft;
   private Vector3 shoulderRightBase = new Vector3(0, -180, -90f);
   private Vector3 shoulderLeftBase = new Vector3(0, 180, 90f);
-  
-    // Start is called before the first frame update b
-    void Start()
+
+
+  /*test line*/
+  public Transform hipRight;
+  public Transform hipLeft;
+  public Transform kneeRight;
+  public Transform kneeLeft;
+  /*test line end*/
+
+  // Start is called before the first frame update b
+  void Start()
     {
       _poseManager=GameObject.Find("PoseManager").GetComponent<PoseManager>();
       
@@ -34,7 +42,7 @@ public class MapYbot : MonoBehaviour
           PoseManager.pose.R_SHOULDER,
           PoseManager.pose.L_SHOULDER,
           PoseManager.pose.L_ELBOW);
-        shoulderLeft.localEulerAngles = shoulderLeftBase + new Vector3(0, -currentFrontAngle, -currentUpAngle);
+      shoulderLeft.localEulerAngles = shoulderLeftBase + new Vector3(0, -currentFrontAngle, -currentUpAngle);
         currentUpAngle = _poseManager.get3DAngle(
           PoseManager.pose.R_HIP,
           PoseManager.pose.R_SHOULDER,
@@ -84,7 +92,72 @@ public class MapYbot : MonoBehaviour
         currentFrontAngle = Vector3.Angle(normalHip, normalForearm);
         elbowRight.localEulerAngles = new Vector3(0,-180,0) + new Vector3(-currentFrontAngle, currentUpAngle, 0);
 
-      Debug.Log(currentUpAngle);
+
+      /*test line*/
+
+      /*
+      currentUpAngle = _poseManager.get3DAngle(
+        PoseManager.pose.L_KNEE,
+        PoseManager.pose.L_HIP, 
+        PoseManager.pose.L_SHOULDER);
+      currentFrontAngle = _poseManager.get3DAngle(
+        PoseManager.pose.R_HIP,
+        PoseManager.pose.L_HIP,
+        PoseManager.pose.L_SHOULDER);
+      var currentSideAngle = _poseManager.get3DAngle(
+        PoseManager.pose.R_HIP,
+        PoseManager.pose.L_HIP,
+        PoseManager.pose.L_KNEE);
+      hipLeft.localEulerAngles = new Vector3(currentUpAngle, currentFrontAngle, currentSideAngle);
+      Debug.Log(hipLeft.localEulerAngles);
+      //hipLeft.localEulerAngles = new Vector3(0, currentFrontAngle, currentUpAngle);
+      currentUpAngle = _poseManager.get3DAngle(
+        PoseManager.pose.R_SHOULDER,
+        PoseManager.pose.R_HIP,
+        PoseManager.pose.R_KNEE);
+      currentFrontAngle = _poseManager.get3DAngle(
+        PoseManager.pose.L_HIP,
+        PoseManager.pose.R_HIP,
+        PoseManager.pose.R_KNEE);
+      hipRight.localEulerAngles = new Vector3(0, currentFrontAngle, currentUpAngle);
+      
+
+      currentUpAngle = _poseManager.get3DAngle(
+          PoseManager.pose.L_HIP,
+          PoseManager.pose.L_KNEE,
+          PoseManager.pose.L_ANKLE);
+      
+      var kneePoint = _poseManager.getRawPoint(PoseManager.pose.L_KNEE);
+      var anklePoint = _poseManager.getRawPoint(PoseManager.pose.L_ANKLE);
+      hipPoint = _poseManager.getRawPoint(PoseManager.pose.L_HIP);
+      vector1 = hipPoint - kneePoint;
+      vector2 = shoulderPoint - kneePoint;
+      normalHip = Vector3.Cross(vector1, vector2);
+      vector1 = hipPoint - kneePoint;
+      vector2 = anklePoint - kneePoint;
+      normalForearm = Vector3.Cross(vector1, vector2);
+      currentFrontAngle = Vector3.Angle(normalHip, normalForearm);
+      kneeLeft.localEulerAngles =new Vector3(-currentFrontAngle, -currentUpAngle, 0);
+
+      currentUpAngle = _poseManager.get3DAngle(
+          PoseManager.pose.R_HIP,
+          PoseManager.pose.R_KNEE,
+          PoseManager.pose.R_ANKLE);
+
+      kneePoint = _poseManager.getRawPoint(PoseManager.pose.R_KNEE);
+      anklePoint = _poseManager.getRawPoint(PoseManager.pose.R_ANKLE);
+      hipPoint = _poseManager.getRawPoint(PoseManager.pose.R_HIP);
+      vector1 = hipPoint - kneePoint;
+      vector2 = shoulderPoint - kneePoint;
+      normalHip = Vector3.Cross(vector1, vector2);
+      vector1 = hipPoint - kneePoint;
+      vector2 = anklePoint - kneePoint;
+      normalForearm = Vector3.Cross(vector1, vector2);
+      currentFrontAngle = Vector3.Angle(normalHip, normalForearm);
+      kneeRight.localEulerAngles =new Vector3(-currentFrontAngle, -currentUpAngle, 0);
+      */
+      /*test line end*/
+      //Debug.Log(currentUpAngle);
     }
-    }
+  }
 }
